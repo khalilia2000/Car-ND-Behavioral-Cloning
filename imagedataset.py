b0d4286f6e5d12926ff765c81258a075b5d30d37
unknown <Ali.Khalili@TTD-GZ80J02.tt.local>
2017-01-03 18:17:19 -0800

    feat: write data generator and move ImgDataSet class to it's own file

---
+++ imagedataset.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  2 13:03:29 2017
+
+@author: Ali Khalili
+"""
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from matplotlib import gridspec
+
+
+class ImgDataSet(object):
+
+  def __init__(self,images,labels,norm_max_min=0.005,scaled_dim=(100,50)):
+               
+    """
+    Construct a DataSet of Images.
+    """
+        
+    assert images.shape[0] == labels.shape[0], (
+        'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+    
+    self._num_examples = images.shape[0]
+    self._images = images
+    self._labels = labels
+    self._size_factor = 3
+    self._aspect_ratio = 1.0 * images.shape[1] / images.shape[2]
+    self._normalization_factor = 1 / norm_max_min
+    self._resize_dim = scaled_dim
+    
+    # The following parameters will be used for retrieving data batches    
+    self._epochs_completed = 0
+    self._index_in_epoch = 0
+
+  @property
+  def images(self):
+    return self._images
+
+  @property
+  def labels(self):
+    return self._labels
+
+  @property
+  def num_examples(self):
+    return self._num_examples
+
+
+  def add_flipped(self):
+    """
+    Adds horizontally flipped images to the dataset
+    """
+    self._images = np.append(self._images, np.asarray([cv2.flip(image,1) for image in self._images]), axis=0)
+    self._labels = np.append(self._labels, np.asarray([-steer_angle for steer_angle in self._labels]), axis=0)
+    self._num_examples = self._images.shape[0]
+    pass  
+
+
+  def color_to_gray(self):
+    """
+    Changes the color to grayscale (i.e. turns images to only one channel)
+    """
+    self._images = np.asarray([cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) for img in self._images])
+    pass
+
+  
+  def equalize_hist(self):
+    """
+    Equalizes the histogram
+    """
+    self._images = np.asarray([cv2.equalizeHist(img) for img in self._images])
+    pass
+
+
+  def crop_top(self):
+    """
+    Crops the top portion of the image
+    """
+    if len(self._images[0].shape) == 2:
+      self._images = np.asarray([img[-100:,:] for img in self._images])
+    else:
+      self._images = np.asarray([img[-100:,:,:] for img in self._images])
+    pass
+  
+
+  def resize(self):
+    """
+    Resizes the images
+    """
+    self._images = np.asarray([cv2.resize(img, self._resize_dim) for img in self._images])
+    pass
+
+  
+  def normalize(self):
+    """
+    Normalizes the piexel values of the images
+    """
+    self._images = self._images.astype('float32')
+    self._images -= 127.5
+    self._images /= 127.5
+    self._images /= self._normalization_factor
+    pass
+  
+  
+  def pre_process(self):
+    """
+    Executes the pipeline for preprocessing the images
+    """
+    self.add_flipped()
+    self.color_to_gray()
+    self.crop_top()
+    self.resize()
+    self.equalize_hist()
+    self.normalize()
+    pass
+  
+  
+  def next_batch(self, batch_size):
+    """
+    Return the next "batch_size" examples from this data set.
+    """
+    start = self._index_in_epoch
+    self._index_in_epoch += batch_size
+    if self._index_in_epoch > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+      # Start next epoch 
+      # No need for suffling the data as generator shuffles the files before loading
+      start = 0
+      self._index_in_epoch = batch_size
+      assert batch_size <= self._num_examples
+    end = self._index_in_epoch
+    return self._images[start:end], self._labels[start:end]
+ 
+
+  def plot_image(self, ax_list, grid_fig, grid_index, image_index, show_label=True, cmap=None):
+    """
+    plots one image in the grid space and passess the appended axis list
+    """
+    ax_list.append(plt.subplot(grid_fig[grid_index]))
+    
+    ax_list[-1].imshow(self._images[image_index], cmap=cmap)
+    ax_list[-1].axis('off')
+    ax_list[-1].set_aspect('equal')
+    y_lim = ax_list[-1].get_ylim()
+    if show_label:
+      ax_list[-1].text(0,int(-1*y_lim[0]*0.1),'Steering Angle = {}'.format(self._labels[image_index]),color='r')
+      ax_list[-1].text(0,int(-1*y_lim[0]*0.3),'Image Index = {}'.format(image_index),color='b')
+
+    return ax_list
+
+  
+  def plot_random_grid(self, n_rows, n_cols, show_labels=True, cmap=None):
+    """
+    plots a random grid of images to verify
+    """
+    
+    g_fig = gridspec.GridSpec(n_rows,n_cols) 
+    g_fig.update(wspace=0.5, hspace=0.75)
+    
+    # setting up the figure
+    plt.figure(figsize=(n_cols*self._size_factor,n_rows*self._size_factor*self._aspect_ratio))
+    selection = np.random.choice(self._num_examples, n_rows*n_cols, replace=False)
+    
+    ax_list = []
+    for i in range(n_rows*n_cols):
+      ax_list = self.plot_image(ax_list, g_fig, i, selection[i], cmap=cmap)
+        
+      
--- model.py
+++ model.py
@@ -1,149 +1,70 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan  2 13:03:29 2017
-
-@author: Ali
-"""
-
+from imagedataset import ImgDataSet
 import numpy as np
-import cv2
-from matplotlib import pyplot as plt
-from matplotlib import gridspec
 import glob
+import cv2
+import pandas as pd
 
 
-class ImgDataSet(object):
-
-  def __init__(self,images,labels,norm_max_min=0.005,scaled_dim=(100,50)):
-               
-    """
-    Construct a DataSet of Images.
-    """
-        
-    assert images.shape[0] == labels.shape[0], (
-        'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
-    
-    self._num_examples = images.shape[0]
-    self._images = images
-    self._labels = labels
-    self._size_factor = 3
-    self._aspect_ratio = 1.0 * images.shape[1] / images.shape[2]
-    self._normalization_factor = 1 / norm_max_min
-    self._resize_dim = scaled_dim
-
-
-  @property
-  def images(self):
-    return self._images
-
-  @property
-  def labels(self):
-    return self._labels
-
-  @property
-  def num_examples(self):
-    return self._num_examples
-
-
-  def add_flipped(self):
-    self._images = np.append(self._images, np.asarray([cv2.flip(image,1) for image in self._images]), axis=0)
-    self._labels = np.append(self._labels, np.asarray([-steer_angle for steer_angle in self._labels]), axis=0)
-    self._num_examples = self._images.shape[0]
-    pass  
-
-
-  def color_to_gray(self):
-    self._images = np.asarray([cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) for img in self._images])
-    pass
-
+def data_generator(num_images_to_load, batch_size):
   
-  def equalize_hist(self):
-    """
-    Equalizes the histogram
-    """
-    self._images = np.asarray([cv2.equalizeHist(img) for img in self._images])
-    pass
-
+  # at least batch_size number of images should be loaded into memory each time  
+  assert num_images_to_load > batch_size
   
-  def normalize(self):
-    """
-    Normalizes the piexel values of the images
-    """
-    self._images = self._images.astype('float32')
-    self._images -= 127.5
-    self._images /= 127.5
-    self._images /= self._normalization_factor
-
-
-  def crop_top(self):
-    """
-    Crops the top portion of the image
-    """
-    if len(self._images[0].shape) == 2:
-      self._images = np.asarray([img[-100:,:] for img in self._images])
-    else:
-      self._images = np.asarray([img[-100:,:,:] for img in self._images])
-    pass
+  # setting up variables and dataframes
+  img_dir = 'C:\\Users\\ali.khalili\\Desktop\\Car-ND\\Car-ND-Behavioral-Cloning-P3\\data\\'    
+  file_names = np.asarray([f_name for f_name in glob.glob(img_dir+'IMG\\'+'*.jpg')])
+  total_images = len(file_names)
+  csv_data = pd.read_csv(img_dir+'driving_log.csv')
   
+  # removing starting spaces
+  csv_data['right'] = csv_data['right'].str.lstrip()
+  csv_data['left'] = csv_data['left'].str.lstrip()
+  csv_data['center'] = csv_data['center'].str.lstrip()
   
-  def resize(self):
-    """
-    Resizes the images
-    """
-    self._images = np.asarray([cv2.resize(img, self._resize_dim) for img in self._images])
-    pass
+  while 1:     
+    # Shuffling the images before loading
+    perm = np.arange(len(file_names))
+    np.random.shuffle(perm)
+    file_names = file_names[perm]    
     
-
-  def plot_image(self, ax_list, grid_fig, grid_index, image_index, show_label=True, cmap=None):
-    """
-    plots one image in the grid space and passess the appended axis list
-    """
-    ax_list.append(plt.subplot(grid_fig[grid_index]))
-    
-    ax_list[-1].imshow(self._images[image_index], cmap=cmap)
-    ax_list[-1].axis('off')
-    ax_list[-1].set_aspect('equal')
-    if show_label:
-      ax_list[-1].text(0,-15,'Steering Angle = {}'.format(self._labels[image_index]),color='r')
-      ax_list[-1].text(0,-45,'Image Index = {}'.format(image_index),color='b')
-
-    return ax_list
-
-  
-  def plot_random_grid(self, n_rows, n_cols, show_labels=True, cmap=None):
-    """
-    plots a random grid of images to verify
-    """
-    
-    g_fig = gridspec.GridSpec(n_rows,n_cols) 
-    g_fig.update(wspace=0.5, hspace=0.75)
-    
-    # setting up the figure
-    plt.figure(figsize=(n_cols*self._size_factor,n_rows*self._size_factor*self._aspect_ratio))
-    selection = np.random.choice(self._num_examples, n_rows*n_cols, replace=False)
-    
-    ax_list = []
-    for i in range(n_rows*n_cols):
-      ax_list = self.plot_image(ax_list, g_fig, i, selection[i], cmap=cmap)
-        
+    # loading all images into memory and passing on to the optimizer in batches
+    start_index = 0
+    while start_index < total_images-1:
+      # loading images and labels data into memory  
+      loaded_images = []
+      labels = []
+      num_acutally_loaded = min(num_images_to_load,total_images-start_index)
+      for i in range(num_acutally_loaded): 
+        loaded_images.append(cv2.imread(file_names[start_index+i]))
+        file_key = 'IMG/' + (file_names[start_index+i]).replace(img_dir+'IMG\\','')
+        cam_type = file_key[file_key.index('/')+1:file_key.index('_')]
+        if cam_type == 'center':
+          steering_offset = 0
+        elif cam_type == 'left':
+          steering_offset = 0.25
+        else:
+          steering_offset = -0.25
+        labels.append(csv_data[csv_data[cam_type]==file_key]['steering'].values[0]+steering_offset)
       
+      # changing loaded images to ndarray
+      loaded_images = np.asarray(loaded_images)
+      labels = np.asarray(labels)
+      start_index += num_acutally_loaded      
+      
+      # creating image dataset and pre-processing the images
+      img_data_set = ImgDataSet(loaded_images, labels)
+      img_data_set.pre_process()
+      
+      # passing on batches of data
+      num_batches = num_acutally_loaded // batch_size
+      for i in range(num_batches):
+        yield img_data_set.next_batch(batch_size)
+
+
 
 def main():
-  img_dir = 'C:\\Udacity Courses\\Car-ND-Udacity\\P3 - Behavioral Cloning\\data\\data\\IMG\\'    
-  image_list = []
-  file_names = [f_name for f_name in glob.glob(img_dir+'*.jpg')]
-  num_images_to_load = 10
-  total_images = len(file_names)
-  for i in range(num_images_to_load): 
-    index = int(np.random.uniform()*total_images)
-    im=cv2.imread(file_names[index])
-    image_list.append(im)    
-  
-  image_list = np.asarray(image_list)  
-  labels = np.asarray([1 for x in image_list])
-  #print(image_list.shape)
-  ids1 = ImgDataSet(image_list,labels,1)
-  return ids1
+  pass
+
 
 if __name__ == '__main__':
   main()
